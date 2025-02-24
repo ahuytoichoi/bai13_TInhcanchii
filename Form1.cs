1e1a18aa133fd2f528955440f3dfab6cb8de449e
ahuytoichoi <huy54212410028@bachkhoasaigon.edu.vn>
2025-02-24 14:42:21 +0700

    hoanghuy

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +13,13 @@ namespace bai13_TInhcanchii
 {
     public partial class Form1 : Form
     {
+        string[] CanArr = { "canh", "tan", "nham", "quy", "giap", "at", "Binh", "Dinh", "Mau", "Ky" };
+        string[] ChiArr = { "thin", "ty", "ngo", "mui", "than", "dau", "tuat", "hoi", "ty", "suu", "dan", "mao" };
+        string TinhCan(int Nam) { return CanArr[Nam % 10]; }
+
+        string TinhChi(int Nam) { return ChiArr[Nam % 12]; }
+
+        string TinhCanChi(string Text) { return TinhCan(Convert.ToInt32(Text)) + "" + TinhChi(Convert.ToInt32(Text)); }
         public Form1()
         {
             InitializeComponent();
@@ -19,9 +27,11 @@ namespace bai13_TInhcanchii
 
         private void btnTinh_Click(object sender, EventArgs e)
         {
-            string[] canArr = { "canh", "tan", "nham", "quy", "giap", "at", "Binh", "Dinh", "Mau", "Ky" };
-            string[] chiArr = { "thin", "ty", "ngo", "mui", "than", "dau", "tuat", "hoi", "ty", "suu","dan","mao"};
-            string giaitretrave = canArr[Convert.ToInt32(txtNam.Text) % 10] + "" + chiArr[Convert.ToInt32(txtNam.Text) % 12];
+          txtKQ.Text = TinhCanChi(txtNam.Text);
+            for (int i = 1000; i < 3000; i++) 
+            {
+                lstNam.Items.Add(i + "-" + TinhCanChi(i.ToString()));
+            }    
         }
 
         private void btnExit_Click(object sender, EventArgs e)
